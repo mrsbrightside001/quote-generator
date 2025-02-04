5d237b7cb5c3b75f9459e7bc18676e2ff653de37
mrsbrightside001 <155654171+mrsbrightside001@users.noreply.github.com>
2025-02-04 15:37:09 +0100

    Integrate API, setup quote generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
+  console.log("Quote generated");
   new Typewriter("#quote", {
-    strings: "The quote will go here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
+    cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#user-prompt");
+  let userInputFormatted = userInput.value
+    .toLowerCase()
+    .split(" ")
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(" ");
+
+  let apiKey = "tb561b9af40b32focb07736aa7b1c96e";
+  let prompt = `Generate a profound quote about ${userInput.value}`;
+  let context = `You are an expert on witty, sarcastic and inspirational quotes. You love to write short and profound quotes to inspire people and help them feel energized and motivated. Your mission is to generate an inspiring and wise quote in 2 lines in basic HTML, e.g. <p>this is a quote<p>. Do not use markdown. Make sure to follow the user instructions. Add an author/a famous person of your choice in the fifth line that you credit the quote to in the format <strong>- Coco Chanel on "${userInputFormatted}"</strong>. Choose a famous celebrity, artist, actor, musician other than Coco Chanel.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating quote");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
